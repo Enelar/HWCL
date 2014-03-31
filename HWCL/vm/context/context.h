99c6807448678e7eea417cfa447947c4763e871b
Kirill Berezin <enelar@develop-project.ru>
2014-03-31 14:06:55 +0400

    Move extern_context to private header

--- HWCL/vm/context/context.h
+++ HWCL/vm/context/context.h
@@ -93,68 +93,6 @@ namespace vm
     friend class process;
     struct variable_not_found {};
   };
-
-  struct extern_context : public context
-  {
-  public:
-    void AddLocal(const string &name, void *ptr, VAR_TYPE t)
-    {
-      if (t == NUMBER)
-      {
-        typedef extern_pointer<vm::floating_point> pointer_t;
-        auto p = make_shared<pointer_t>(ptr, t);
-        dynamic_typing.insert({ name, t });
-        pointers.insert({ name, p });
-        return;
-      }
-      if (t == BOOLEAN)
-      {
-        typedef extern_pointer<bool> pointer_t;
-        auto p = make_shared<pointer_t>(ptr, t);
-        dynamic_typing.insert({ name, t });
-        pointers.insert({ name, p });
-        return;
-      }
-      if (t == STRING)
-      {
-        typedef extern_pointer<string> pointer_t;
-        auto p = make_shared<pointer_t>(ptr, t);
-        dynamic_typing.insert({ name, t });
-        pointers.insert({ name, p });
-        return;
-      }
-      if (t == ENUM)
-      {
-        typedef extern_pointer<int> pointer_t;
-        auto p = make_shared<pointer_t>(ptr, t);
-        dynamic_typing.insert({ name, t });
-        pointers.insert({ name, p });
-        return;
-      }
-      dead_space();
-    }
-
-    function<int(string, string)> GetEnumField; REFACTOR
-
-    int EnumWorkAround(const string &namelist, const string &name) override
-    {
-      int ret;
-
-      if (context::EnumWorkAround(namelist, name, ret))
-        return ret;
-      ret = GetEnumField(namelist, name);
-
-      auto i_namelist = enums.find(namelist);
-      if (i_namelist == enums.end())
-      {
-        enums.insert({ namelist, {} });
-        i_namelist = enums.find(namelist);
-        throw_assert(i_namelist != enums.end());
-      }
-      i_namelist->second.insert({ name, ret });
-      return ret;
-    }
-  };
 }
 
 #define HWCL_CONTEXT_DECLARED
@@ -162,5 +100,7 @@ namespace vm
 #include "context.hpp"
 #include "../types/pointer.hpp"
 #endif
+
+#include "extern_context.h"
 
 #endif
---
+++ HWCL/vm/context/extern_context.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "context.h"
+
+namespace vm
+{
+  struct extern_context : public context
+  {
+  public:
+    void AddLocal(const string &name, void *ptr, VAR_TYPE t)
+    {
+      if (t == NUMBER)
+      {
+        typedef extern_pointer<vm::floating_point> pointer_t;
+        auto p = make_shared<pointer_t>(ptr, t);
+        dynamic_typing.insert({ name, t });
+        pointers.insert({ name, p });
+        return;
+      }
+      if (t == BOOLEAN)
+      {
+        typedef extern_pointer<bool> pointer_t;
+        auto p = make_shared<pointer_t>(ptr, t);
+        dynamic_typing.insert({ name, t });
+        pointers.insert({ name, p });
+        return;
+      }
+      if (t == STRING)
+      {
+        typedef extern_pointer<string> pointer_t;
+        auto p = make_shared<pointer_t>(ptr, t);
+        dynamic_typing.insert({ name, t });
+        pointers.insert({ name, p });
+        return;
+      }
+      if (t == ENUM)
+      {
+        typedef extern_pointer<int> pointer_t;
+        auto p = make_shared<pointer_t>(ptr, t);
+        dynamic_typing.insert({ name, t });
+        pointers.insert({ name, p });
+        return;
+      }
+      dead_space();
+    }
+
+    function<int(string, string)> GetEnumField; REFACTOR
+
+    int EnumWorkAround(const string &namelist, const string &name) override
+    {
+        int ret;
+
+        if (context::EnumWorkAround(namelist, name, ret))
+          return ret;
+        ret = GetEnumField(namelist, name);
+
+        auto i_namelist = enums.find(namelist);
+        if (i_namelist == enums.end())
+        {
+          enums.insert({ namelist, {} });
+          i_namelist = enums.find(namelist);
+          throw_assert(i_namelist != enums.end());
+        }
+        i_namelist->second.insert({ name, ret });
+        return ret;
+    }
+  };
+}
