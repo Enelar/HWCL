d54d22dd146df5a25abb48bd95371d41fcf84129
Kirill Berezin <enelar@develop-project.ru>
2014-03-31 19:08:49 +0400

    Labels trying to serialize

--- HWCL/serialization.h
+++ HWCL/serialization.h
@@ -1,30 +1,38 @@
 #include "stdafx.h"
 
-template<typename T>
-struct abstract_serializer
-{
-};
-
 template<typename T, typename Enable = void>
 struct serializer;
 
 template<typename T>
-struct serializer<T, typename enable_if<is_trivially_copyable<T>::value, T>::type>
-  : abstract_serializer<T>
+struct serializer<T, typename enable_if<is_const<T>::value>::type>
+  : serializer<typename remove_const<T>::type>
 {
-  deque<ub> Se(const T &orig)
+
+};
+
+template<typename T>
+struct serializer<T, typename enable_if<is_trivially_copyable<T>::value>::type>
+{
+  static deque<ub> Se(const T &orig)
   {
     deque<ub> ret;
-    ub *begin = &orig;
+    const ub *begin = (const ub *)&orig;
     copy_n(begin, sizeof(T), ret.begin());
     return ret;
   }
 
-  T De(const deque<ub> &orig)
+  static T De(const deque<ub> &orig)
   {
     vector<ub> align(orig.begin(), orig.end());
     T ret;
     memcpy(&ret, &align[0], sizeof(T));
+    return ret;
+  }
+
+  static T DeR(deque<ub> &orig)
+  {
+    T ret = De(orig);
+    orig.erase(orig.begin(), orig.begin() + sizeof(T));
     return ret;
   }
 };
@@ -32,13 +40,59 @@ struct serializer<T, typename enable_if<is_trivially_copyable<T>::value, T>::typ
 template<>
 struct serializer<string>
 {
-  deque<ub> Se(const string &str)
+  static deque<ub> Se(const string &str)
   {
     return{ str.begin(), str.end() };
   }
 
-  string De(const deque<ub> &orig)
+  static string De(const deque<ub> &orig)
   {
     return{ orig.begin(), orig.end() };
+  }
+
+  static string DeR(deque<ub> &orig)
+  {
+    string str = De(orig);
+    orig.clear();
+    return str;
+  }
+};
+
+template<>
+struct serializer<deque<ub>>
+{
+  deque<ub> res;
+
+  deque<ub> Se(const deque<ub> &orig)
+  {
+    auto size = serializer<hword>::Se(orig.size());
+
+    res.insert(res.end(), size.begin(), size.end());
+    res.insert(res.end(), orig.begin(), orig.end());
+
+    return res;
+  }
+
+  deque<ub> De(const deque<ub> &orig)
+  {
+    res.insert(res.end(), orig.begin(), orig.end());
+    return De();
+  }
+
+  template<typename T = deque<ub>>
+  T De()
+  {
+    return serializer<T>::DeR(res);
+  }
+
+  template<>
+  deque<ub> De<deque<ub>>()
+  {
+    throw_assert(res.size() != 0);
+    auto size = serializer<hword>::De(res);
+    res.erase(res.begin(), res.begin() + sizeof(hword));
+    deque<ub> ret{ res.begin(), res.begin() + size };
+    res.erase(res.begin(), res.begin() + sizeof(size));
+    return ret;
   }
 };
--- HWCL/vm/context/labels.cpp
+++ HWCL/vm/context/labels.cpp
@@ -27,3 +27,32 @@ const decltype(labels::data) *labels::operator->() const
 {
   return &data;
 }
+
+labels::labels(const deque<ub> &orig)
+{
+  serializer<deque<ub>> ret{ orig };
+  auto count = ret.De<word>();
+
+  for (word i = 0; i < count; i++)
+  {
+    typedef decltype(data.find("")) pair;
+    auto str = serializer<decltype(declval<pair>()->first)>::De(ret.De());
+    auto val = serializer<decltype(declval<pair>()->second)>::De(ret.De());
+    data.insert({ str, val });
+  }
+}
+
+deque<ub> labels::Serialize() const
+{
+  serializer<deque<ub>> ret;
+
+  ret.Se(serializer<word>::Se(data.size()));
+
+  for (auto el : data)
+  {
+    ret.Se(serializer<decltype(el.first)>::Se(el.first));
+    ret.Se(serializer<decltype(el.second)>::Se(el.second));
+  }
+
+  return ret.res;
+}
